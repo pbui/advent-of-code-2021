89f386193d21e06c0867de29b1c9fbe1cbeb375d
Peter Bui <pbui@bx612.space>
2021-12-01 00:47:49 -0500

    day01: use list comprehensions

--- day01/day01-A.py
+++ day01/day01-A.py
@@ -2,25 +2,13 @@
 
 import sys
 
-# Functions
-
-def count_depth_increases(depths):
-    depth_increases = 0
-    previous_depth  = depths.pop(0)
-
-    while depths:
-        current_depth = depths.pop(0)
-        if current_depth > previous_depth:
-            depth_increases += 1
-        previous_depth = current_depth
-
-    return depth_increases
-
 # Main Execution
 
 def main():
     report    = [int(depth) for depth in sys.stdin]
-    increases = count_depth_increases(report)
+    increases = sum(
+        1 for i in range(1, len(report)) if report[i] > report[i - 1]
+    )
     print(increases)
 
 if __name__ == '__main__':
--- day01/day01-B.py
+++ day01/day01-B.py
@@ -1,41 +1,15 @@
 #!/usr/bin/env python3
 
-import collections
 import sys
-
-# Functions
-
-def count_depth_increases(depths):
-    depth_increases = 0
-    previous_depth  = depths.pop(0)
-
-    while depths:
-        current_depth = depths.pop(0)
-        if current_depth > previous_depth:
-            depth_increases += 1
-        previous_depth = current_depth
-
-    return depth_increases
-
-def group_windows(depths, length=3):
-    windows        = []
-    current_window = collections.deque(maxlen=length)
-
-    while depths:
-        current_depth = depths.pop(0)
-        current_window.append(current_depth)
-
-        if len(current_window) == 3:
-            windows.append(sum(current_window))
-
-    return windows
 
 # Main Execution
 
 def main():
     report    = [int(depth) for depth in sys.stdin]
-    windows   = group_windows(report)
-    increases = count_depth_increases(windows)
+    windows   = [sum(report[i:i+3]) for i in range(0, len(report) - 2)]
+    increases = sum(
+        1 for i in range(1, len(windows)) if windows[i] > windows[i - 1]
+    )
     print(increases)
 
 if __name__ == '__main__':
