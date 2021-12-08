52c210b9f1f8883ff35347401ce0c492782bef93
Peter Bui <pbui@bx612.space>
2021-12-08 00:19:12 -0500

    day08: Part A (Python)

---
+++ day08/day08-A.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import collections
+import sys
+
+# Constants
+
+SEGMENTS_COUNT_TO_DIGIT = {
+    2 : 1,
+    4 : 4,
+    3 : 7,
+    7 : 8,
+}
+
+# Functions
+
+def count_1478_outputs(outputs):
+    return sum(
+        1 if len(collections.Counter(output)) in SEGMENTS_COUNT_TO_DIGIT else 0
+        for output in outputs
+    )
+
+# Main Execution
+
+def main():
+    count_1478_digits = 0
+
+    for entry in sys.stdin:
+        patterns, outputs  = entry.split('|')
+        patterns           = patterns.split()
+        outputs            = outputs.split()
+        count_1478_digits += count_1478_outputs(outputs)
+
+    print(f'Part A: {count_1478_digits}')
+
+if __name__ == '__main__':
+    main()
