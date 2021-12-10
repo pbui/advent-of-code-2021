6d8490daeb56829385833cc4c297cf50b47dbcdf
Peter Bui <pbui@bx612.space>
2021-12-10 01:11:14 -0500

    day10: Part B

--- day10/day10.js
+++ day10/day10.js
@@ -21,6 +21,13 @@ const CLOSING_BRACKETS = {
     '>': 25137,
 };
 
+const COMPLETE_POINTS = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
+};
+
 // Functions
 
 function parse_line(line) {
@@ -31,22 +38,32 @@ function parse_line(line) {
     	    stack.push(bracket);
 	} else {			    // Closing bracket
 	    if (!stack.length) {	    // Incomplete: missing closing bracket
-	    	return 0;
+	    	return [0, stack];
 	    }
 
 	    let previous = stack.pop();
 	    if (bracket != OPENING_BRACKETS[previous]) {
-	    	return CLOSING_BRACKETS[bracket];
+	    	return [CLOSING_BRACKETS[bracket], null];
 	    }
 	}
     }
 
-    return 0;
+    return [0, stack];
+}
+
+function complete_line(line) {
+    return line.reverse().reduce((s, b) => 5*s + COMPLETE_POINTS[b], 0);
 }
 
 // Main Execution
 
 let lines  = IO.readlines(line => line.split(''));
-let part_a = lines.reduce((s, l) => s + parse_line(l), 0);
-
+let part_a = lines.reduce((s, l) => s + parse_line(l)[0], 0);
 IO.print(`Part A: ${part_a}`);
+
+let scores = lines.map(parse_line)
+		  .filter(p => !p[0])
+		  .map(p => complete_line(p[1]))
+		  .sort((a, b) => (a - b));
+let part_b = scores[Math.floor(scores.length / 2)];
+IO.print(`Part B: ${part_b}`);
